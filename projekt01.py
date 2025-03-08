d1da6beba4f4faf438057cdf4b2ad8c7c9756829
RKasprzycki <radoslaw.kasprzycki@edukacja.uwm.edu.pl>
2025-03-08 16:27:59 +0100

    commit PyCharm2

--- projekt01.py
+++ projekt01.py
@@ -1,1 +1,21 @@
-print("Hello3!")
+
+
+# operatory arytmetyczne
+
+# operator dodawania
+print(1 + 2)
+# operator odejmowania
+print(1 - 2)
+# operator mnożenia
+print(1 * 2)
+# operator dzielenia z resztą
+print(1 / 2)
+# operator dzielenia bez reszty (dzielenie całkowite)
+print(1 // 2)
+
+
+
+zmienna = "wartosc"
+print(type(zmienna))
+zmienna=1
+print(type(zmienna))
